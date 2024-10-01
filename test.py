7caee55f32b47d1f1f8e3c4a3af62705b69b32ee
AsifKamalHm <115923499+AsifKamalHm@users.noreply.github.com>
2024-10-02 00:21:18 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-print("Wassup Buddy!! v1...............")
-print("Wassup Buddy........................sajjad ahmad")
-print("Wassup Buddy........................Asif")
+print("Wassup Buddy!! v2...............")
+
 
 
